1fb25c1d4334a60b04a7d6b3df90e05945fedc34
bigotes invisibles <matiasriscolagos@gmail.com>
2023-09-20 18:38:57 +0200

    Init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const songs = [
+  {
+    title: "Hayloft I",
+    Artist: "Mother Mother",
+    genres: "Indie rock",
+    duration: "3:24",
+    yearOfRelease: 2008,
+    wasOnFire: true,
+  },
+  {
+    title: "La posada de los muertos",
+    Artist: "Mägo de Oz",
+    genres: "Metal progresivo",
+    duration: "4:45",
+    yearOfRelease: 2005,
+    wasOnFire: true,
+  },
+  {
+    title: "Vitality",
+    Artist: "Mittsies",
+    genres: "Instrumental",
+    duration: "3:37",
+    yearOfRelease: 2020,
+    wasOnFire: false,
+  },
+];
