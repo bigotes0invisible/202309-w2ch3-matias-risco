219069961ff525d737cb504050585d5f1b59872f
bigotes invisibles <matiasriscolagos@gmail.com>
2023-09-20 19:32:35 +0200

    add function findSongWithTitle

--- index.js
+++ index.js
@@ -40,11 +40,13 @@ const deleteFromSongsWithTitle = (title) => {
   let index = 0;
   songs.find((song, position) => {
     index = position;
-    return song.title == title;
+    return song.title === title;
   });
   songs.splice(index, index);
 };
 
+const findSongWithTitle = (title) => songs.find((song) => song.title === title);
+
 songs.forEach((song) => console.log(song.title));
-deleteFromSongsWithTitle("La posada de los muertos");
+console.log(findSongWithTitle("La posada de los muertos"));
 songs.forEach((song) => console.log(song.title));
